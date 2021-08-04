51be90a359f6b33fb757e6dc91590d0c9fa16371
yingcongleong <yingcongleong12179@gmail.com>
2021-08-04 22:05:24 +0800

    slider + filter

--- client/src/page/user/HydraMap2.jsx
+++ client/src/page/user/HydraMap2.jsx
@@ -50,6 +50,9 @@ import { LayerList } from '../../lib/api'
 import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import Slider from '@material-ui/core/Slider';
 
+import TooltipMaterial from '@material-ui/core/Tooltip';
+import { DataFilterExtension } from '@deck.gl/extensions';
+
 const ShowWrapper = styled.div(
   props => (
     {
@@ -148,28 +151,75 @@ export const LogLatBar = styled.div(
   )
 )
 
-export function CheckItem({ data, onChange }) {
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
 
-  const [timeList,setTimeList] = useState([])
+  return (
+    <TooltipMaterial open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </TooltipMaterial>
+  );
+}
 
-  
 
-  /* useEffect(()=>{
+const hashCode = (str) => { // java String#hashCode
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
+
+const getDotColor = d => {
+  let a = hashCode(d.name)
+  return [a & 255, (a >> 1) & 255, (a >> 2) & 255]
+};
+
+
+export function CheckItem({ data, currentLayer, setCurrentLayer, onChange, originData }) {
+
+  const [timeList, setTimeList] = useState([])
+  const [min, setmin] = useState(0)
+  const [max, setmax] = useState(0)
+  const [currentSliderValue, setCurrentSliderValue] = useState(0)
+
+  function findminmax(arr) {
+    setmin(arr[0].value)
+    setmax(arr[arr.length - 1].value)
+  }
+
+  useEffect(() => {
     let dayjs = require("dayjs")
     let newTimeList = []
-    if(data.time_serie){
-      data.data.features.forEach((element)=>{
-        let time = dayjs(element.properties.time)
-        newTimeList.push({
-          value: time.valueOf(),
-          label: time.valueOf(),
-        })
+    if (data.time_serie) {
+      data.data.features.forEach((element) => {
+        newTimeList.push(element.properties.time)
       })
+      newTimeList = newTimeList.sort()
       let uniq = []
       let unique = [...new Set(newTimeList)];
+      unique.forEach((element) => {
+        let time = dayjs(element)
+        uniq.push({
+          value: time.valueOf()
+        })
+      })
       setTimeList(uniq)
+      findminmax(uniq)
+      setCurrentSliderValue(uniq[0].value)
     }
-  }) */
+  }, [data])
+
+  function valueLabelFormat(value) {
+    let dayjs = require("dayjs")
+    return dayjs(value).format('YYYY/MM/DD')
+  }
+  
+  
+  const handleChange = (event, newValue) => {
+    setCurrentSliderValue(newValue)
+    setCurrentLayer(newValue)
+  };
 
   return (
     <InputWrapper>
@@ -184,9 +234,17 @@ export function CheckItem({ data, onChange }) {
         </div>
         {data.time_serie && <>
           <Slider
+            value={currentSliderValue}
+            min={min}
+            max={max}
             aria-labelledby="discrete-slider"
             valueLabelDisplay="auto"
+            step={null}
+            defaultValue={min}
             marks={timeList}
+            onChange={handleChange}
+            valueLabelFormat={valueLabelFormat}
+            ValueLabelComponent={ValueLabelComponent}
           />
         </>
         }
@@ -200,9 +258,10 @@ export function CheckItem({ data, onChange }) {
 function Layer({ layers, setLayers, setHoverInfo }) {
 
   const { t, i18n } = useTranslation();
-  const [AllData, setAllData] = useState([])
-  const [currentDataIdx, setCurrentDataIdx] = useState(0)
-  const [currentSelectedData, setCurrentSelectedData] = useState([])
+  const [originData, setOriginData] = useState([]) //原本的不會修改到的data
+  const [AllData, setAllData] = useState([]) //地圖顯示Data
+  const [currentDataIdx, setCurrentDataIdx] = useState(0) //選擇的json檔分類
+  const [currentSelectedData, setCurrentSelectedData] = useState([]) //右邊列表顯示的json檔
 
   const onChangeCurrentData = (idx) => {
     setCurrentDataIdx(idx)
@@ -213,20 +272,7 @@ function Layer({ layers, setLayers, setHoverInfo }) {
     setHoverInfo(data)
   }
 
-  const hashCode = (str) => { // java String#hashCode
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-  }
-
-  const getDotColor = d => {
-    let a = hashCode(d.name)
-    return [a & 255, (a >> 1) & 255, (a >> 2) & 255]
-  };
-
-  const OnListItemsChange = (e, data, index) => { //data是從ylchecked裡取出
+  const OnListItemsChange = (e, data, index) => {
     const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value //CheckBox打勾是True沒打勾是False
     let newMapData = [...AllData]
     newMapData[currentDataIdx].files[index].value = value
@@ -270,10 +316,6 @@ function Layer({ layers, setLayers, setHoverInfo }) {
     setLayers(newLayer) //修改本來地圖的layer */ 
   }
 
-  let CurrentListItems = currentSelectedData.map((data, index) =>
-    <CheckItem data={AllData[currentDataIdx].files[index]} onChange={(e) => OnListItemsChange(e, data, index)} />
-  );
-
   useEffect(() => {
     LayerList().then((res) => {
       let list = []
@@ -291,6 +333,7 @@ function Layer({ layers, setLayers, setHoverInfo }) {
         })
       })
       setAllData(list)
+      setOriginData(list)
       setCurrentSelectedData(list[currentDataIdx].files)
 
       let newLayer = []
@@ -349,9 +392,63 @@ function Layer({ layers, setLayers, setHoverInfo }) {
     })
   }, [])
 
-  useEffect(() => {
+  function getLayer(data) {
+    layers.forEach(element => {
+      if (element.props.name === data.name) {
+        return element
+      }
+    })
+  }
 
-  })
+  useEffect(()=>{
+    console.log(layers)
+  },[layers])
+
+  function setCurrentLayer(data, time) {
+    console.log(layers)
+    let newLayer = [...layers] //複製一個layer
+    let dayjs = require("dayjs")
+
+    const getFilterValue = (d) => {
+      const dayjs = require("dayjs")
+      let time = dayjs(d.properties.time).valueOf()
+      return time
+    }
+
+    newLayer.forEach((element,i) => {
+      if (element.props.name === data.name) {
+        newLayer[i] = new GeoJsonLayer({
+            id: data.name,
+            name: data.name,
+            data: data.data,
+            visible: data.value,
+            // Styles
+            filled: true,
+            pointRadiusMinPixels: 2,
+            pointRadiusScale: 5,
+            getPointRadius: f => 5,
+            getFillColor: getDotColor(data),
+            // Interactive props
+            pickable: true,
+            autoHighlight: true,
+            onHover: onHover,
+            filterEnabled:true,
+            getFilterValue: getFilterValue,
+            filterTransformSize: true,
+            filterTransformColor: true,          
+            filterRange: [time-10 , time+10],
+            // Define extensions
+            extensions: [new DataFilterExtension({filterSize: 1})]
+          })
+      }
+    })
+    console.log(time)
+    setLayers(newLayer)
+  }
+
+  let CurrentListItems = currentSelectedData.map((data, index) =>
+    <CheckItem currentLayer={getLayer(data)} setCurrentLayer={(time) => setCurrentLayer(data, time)} originData={originData} data={AllData[currentDataIdx].files[index]} onChange={(e) => OnListItemsChange(e, data, index)} />
+  );
 
   let BtnList = AllData.map((data, index) =>
     <NormalButton className={styles.btn_list} isLightOn={currentDataIdx === data.id} text={data.name} onClick={(e) => onChangeCurrentData(index)} />
